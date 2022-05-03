c1948bbcde6764aa73c3bdb408a6347fd4531e79
Takphantom <miroterra@naver.com>
2022-05-03 23:27:24 +0900

    ejs 초기

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Node.js 공부2
 
+## 내용 정리
+
+    app.use(express.static('public'));
+
+**정적 파일**(HTML을 제외하고 웹 페이지를 렌더링 할 때 필요한 추가적인 파일)을 제공하는 미들웨어 함수
+
     app.use(express.urlencoded({ extended: false }));
 
 false이면 기본으로 내장된 querystring 모듈 사용
--- app.js
+++ app.js
@@ -1,34 +1,65 @@
+const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// 이 앱에 대한 특정 옵션 설정
+
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+  // res.sendFile(htmlFilePath);
+  // ejs 사용으로 변경
+  res.render('index');
 });
 
 app.get('/restaurants', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+  // res.sendFile(htmlFilePath);
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render('restaurants', { numberOfRestaurants: storedRestaurants.length });
 });
 
 app.get('/recommend', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+  // res.sendFile(htmlFilePath);
+  res.render('recommend');
+});
+
+app.post('/recommend', function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirm');
 });
 
 app.get('/confirm', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+  // res.sendFile(htmlFilePath);
+  res.render('confirm');
 });
 
 app.get('/about', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'about.html');
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+  // res.sendFile(htmlFilePath);
+  res.render('about');
 });
 
 app.listen(3000);
