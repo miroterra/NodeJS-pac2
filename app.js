639dc626c98f6db9c9347ba1ea0fe0d3e6d9f590
Takphantom <miroterra@naver.com>
2022-05-08 16:03:48 +0900

    detail id 축

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const uuid = require('uuid');
 
 const app = express();
 
@@ -30,6 +31,21 @@ app.get('/restaurants', function (req, res) {
   res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
 });
 
+//클릭한 식당의 상세정보
+app.get('/restaurants/:id', function (req, res) {
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render('restaurant-detail', { restaurant: restaurant });
+    }
+  }
+});
+
 app.get('/recommend', function (req, res) {
   // const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
   // res.sendFile(htmlFilePath);
@@ -38,6 +54,7 @@ app.get('/recommend', function (req, res) {
 
 app.post('/recommend', function (req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
